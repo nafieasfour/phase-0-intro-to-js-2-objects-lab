ac3ccf5b442cc9226a32b84ce80082b8f43549ad
Nafie Asfour <ph.nafee.asfour@gmail.com>
2023-06-06 17:44:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+let employee = {
+    name: "Ali",
+    streetAddress : "Abc140"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let newemployee = {...employee};
+    newemployee[key] = value;
+    return newemployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key, value){
+    let employeee = {...employee};
+    delete employeee[key];
+    return employeee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key, value){
+    delete employee[key];
+    return employee;
+}
